1317347b50e325410de4337f3997ca35710e73ca
st <e105725@gmail.com>
2014-05-19 21:54:12 +0900

    init

---
+++ JavaFX_Vending_Machine/src/first/VendingMachine.java
@@ -0,0 +1,17 @@
+package first;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class VendingMachine extends Application {
+	public static void main(String[] args) {
+		launch();
+	}
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
